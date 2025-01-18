f4a075b5b5808c34ae7c24ad64966eb9ceec5146
Conjurer-xx <uljanov@gmail.com>
2025-01-18 16:27:41 +0200

    initial project

---
+++ src/test/java/com/att/acceptance/movie_theater/MovieTheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.att.acceptance.movie_theater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
